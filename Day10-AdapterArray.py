73bd841c1bad8cb607ac63bf00b56163f7f43a67
Nathan Holmes <naholmes93@gmail.com>
2020-12-10 08:39:38 +0000

    Day10, nice bit of dynamic programming

---
+++ Day10-AdapterArray.py
@@ -0,0 +1,35 @@
+from collections import Counter
+
+def diff_counts(values : list[int]) -> dict[int, int]:
+    """Count the gaps between ordered elements in a list, by size."""
+    ordered = [0] + sorted(values) + [max(values) + 3]
+
+    return Counter(j - i for i, j in zip(ordered, ordered[1:]))
+
+def count_orders(values : list[int], max_diff : int) -> int:
+    """Count the valid progressions through the values with a maximum jump size."""
+    ordered = [0] + sorted(values) + [max(values) + 3]
+    options = [0] * len(ordered)
+    options[0] = 1
+
+    # Each element of options should be the sum of all prior elements
+    # where the difference between the matching values in ordered is less than
+    # max_diff
+    for i in range(1, len(ordered)):
+        j = i-1
+        while j >= 0:
+            if ordered[i] - ordered[j] > max_diff:
+                break # difference too large
+            options[i] += options[j]
+            j -= 1
+    
+    return options[-1]
+
+
+if __name__ == "__main__":
+    with open("input10.txt") as f:
+        values = [*map(int, f.readlines())]
+    
+    diffs = diff_counts(values)
+    print("Diffs in chain:", diffs, diffs[1] * diffs[3])
+    print("Options for chain:", count_orders(values, 3))
